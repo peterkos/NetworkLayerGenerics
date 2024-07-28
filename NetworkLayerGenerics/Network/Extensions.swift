b70f8a9cd4bbed83cca123314b891b49c3ce4ca0
Peter Kos <pkos91@icloud.com>
2024-07-27 22:24:24 -0700

    Cleanup extensions

--- NetworkLayerGenerics/Network/Extensions.swift
+++ NetworkLayerGenerics/Network/Extensions.swift
@@ -9,21 +9,9 @@ import Foundation
 
 // MARK: - Helpers
 
-extension URLResponse {
-    var httpURLResponse: HTTPURLResponse {
-        // SAFETY: Quinn the Eskimo's blessing
-        // https://forums.developer.apple.com/forums/thread/120099?answerId=372749022#372749022
-        self as! HTTPURLResponse
-    }
-}
-
 extension HTTPURLResponse {
     var responseCode: ResponseCode? {
-        ResponseCode(rawValue: httpURLResponse.statusCode)
-    }
-
-    var responseCodeIsError: Bool? {
-        responseCode?.isError
+        ResponseCode(rawValue: statusCode)
     }
 }
 
@@ -32,6 +20,8 @@ extension URLSession {
     /// Not sure why this isn't in the standard library yet.
     func dataResponse(for request: URLRequest) async throws -> (Data, HTTPURLResponse) {
         let (data, response) = try await URLSession.shared.data(for: request)
-        return (data, response.httpURLResponse)
+        // SAFETY: Quinn the Eskimo's blessing
+        // https://forums.developer.apple.com/forums/thread/120099?answerId=372749022#372749022
+        return (data, response as! HTTPURLResponse)
     }
 }
