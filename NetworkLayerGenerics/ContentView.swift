efc2aa89b699ca9bbdf8cfa036284ea78cafa623
Peter Kos <pkos91@icloud.com>
2024-07-27 21:55:00 -0700

    Initial Commit

---
+++ NetworkLayerGenerics/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NetworkLayerGenerics
+//
+//  Created by Peter Kos on 7/27/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NetworkLayerGenerics/NetworkLayerGenericsApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkLayerGenericsApp.swift
+//  NetworkLayerGenerics
+//
+//  Created by Peter Kos on 7/27/24.
+//
+
+import SwiftUI
+
+@main
+struct NetworkLayerGenericsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
