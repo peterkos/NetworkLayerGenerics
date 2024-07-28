6ea5dd1acbfc354e83e5ca80c53965f37c22e5c0
Peter Kos <pkos91@icloud.com>
2024-07-27 21:55:38 -0700

    start

---
+++ NetworkLayerGenerics/RootView.swift
@@ -0,0 +1,18 @@
+//
+//  RootView.swift
+//  NetworkLayerGenerics
+//
+//  Created by Peter Kos on 7/27/24.
+//
+
+import SwiftUI
+
+struct RootView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    RootView()
+}
